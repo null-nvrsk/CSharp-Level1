a1da83f62402a53aff0a61616aaec6832fdb0611
NULL <nullnvrsk@gmail.com>
2020-07-12 13:07:48 +0300

    Add 2 tasks for 3 lesson

---
+++ 1_Complex/Complex.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _1_Complex
+{
+    class Complex
+    {
+        public double a, b;
+
+        //---------------------------------------------------------------------
+        public Complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        //---------------------------------------------------------------------
+        public override string ToString()
+        {
+            return (b >= 0) ? $"{a} + {b}i" : $"{a} - {-b}i";
+        }
+
+        //---------------------------------------------------------------------
+        public static Complex Plus(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: z1.a + z2.a,
+                b: z1.b + z2.b
+            );
+        }            
+        
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: z1.a + z2.a,
+                b: z1.b + z2.b
+            );
+        }
+
+        //---------------------------------------------------------------------
+        public static Complex Sub(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: z1.a - z2.a,
+                b: z1.b - z2.b
+            );
+        }             
+        
+        public static Complex operator -(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: z1.a - z2.a,
+                b: z1.b - z2.b
+            );
+        }        
+        
+        //---------------------------------------------------------------------
+        public static Complex Multi(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: (z1.a * z2.a) - (z1.b * z2.b),
+                b: (z1.a * z2.b) + (z1.b * z2.a)
+            );
+        }
+
+        public static Complex operator *(Complex z1, Complex z2)
+        {
+            return new Complex(
+                a: (z1.a * z2.a) - (z1.b * z2.b),
+                b: (z1.a * z2.b) + (z1.b * z2.a)
+            );
+        }
+    }
+}
---
+++ 1_Complex/Program.cs
@@ -0,0 +1,77 @@
+﻿// [Скоморохов Максим]
+// б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+// в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Complex
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userChoice = 0;
+
+            while (GetUserChoice(ref userChoice))
+            {
+                // вывод диалогового окна для ввода значений
+                Console.WriteLine("1ое комплексное число: a + bi");
+                Console.Write("a = ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("b = ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("2ое комплексное число: c + di");
+                Console.Write("c = ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("d = ");
+                double d = Convert.ToDouble(Console.ReadLine());
+
+
+                Complex z1 = new Complex(a, b);
+                Complex z2 = new Complex(c, d);
+
+                switch (userChoice)
+                {
+                    case 1: // сложение
+                        Complex result = z1 + z2;
+                        Console.WriteLine($"Сложение: {result}");
+                        break;
+                    case 2: // вычитание
+                        result = z1 - z2;
+                        Console.WriteLine($"Вычитание: {result}");
+                        break;
+                    case 3: // Умножение
+                        result = z1 * z2;
+                        Console.WriteLine($"Умножение: {result}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверное число! Введите число от 0 до 3");
+                        break;
+                }
+                Console.WriteLine("--------------------------------");
+            }
+ 
+        }
+
+        //---------------------------------------------------------------------
+        // вывод диалогового окна для выбора
+        static bool GetUserChoice(ref int userChoice)
+        {
+            Console.WriteLine("Введите номера варианта операции с комплексными числами");
+            Console.WriteLine("1 - сложение\n2 - вычитание\n3 - умножение\nлюбое другое число - выход");
+            userChoice = Convert.ToInt32(Console.ReadLine());
+
+            return (userChoice > 0 && userChoice <= 3) ? true : false;
+        }
+    }
+}
