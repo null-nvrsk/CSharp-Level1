911c41804d8973f17046432917726d0d97b71ae8
NULL <nullnvrsk@gmail.com>
2020-08-01 14:05:01 +0300

    add 2 tasks for 7 lesson

---
+++ 1_Doubler/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace _1_Doubler
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblCommands = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblFindNumber = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(22, 21);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "label1";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(165, 16);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+1";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(165, 46);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(75, 23);
+            this.btnDouble.TabIndex = 2;
+            this.btnDouble.Text = "x2";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(165, 76);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblCommands
+            // 
+            this.lblCommands.AutoSize = true;
+            this.lblCommands.Location = new System.Drawing.Point(22, 51);
+            this.lblCommands.Name = "lblCommands";
+            this.lblCommands.Size = new System.Drawing.Size(94, 13);
+            this.lblCommands.TabIndex = 4;
+            this.lblCommands.Text = "Кол-во команд: 0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(248, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // lblFindNumber
+            // 
+            this.lblFindNumber.AutoSize = true;
+            this.lblFindNumber.Location = new System.Drawing.Point(22, 81);
+            this.lblFindNumber.Name = "lblFindNumber";
+            this.lblFindNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblFindNumber.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 110);
+            this.Controls.Add(this.lblFindNumber);
+            this.Controls.Add(this.lblCommands);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Удвоитель";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblCommands;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.Label lblFindNumber;
+    }
+}
+
---
+++ 2_GuessNumber/MainForm.cs
@@ -0,0 +1,90 @@
+﻿// [Скоморохов Максим]
+// Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток.Для ввода данных от человека используется элемент TextBox.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_GuessNumber
+{
+    public partial class MainForm : Form
+    {
+        private int number = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //---------------------------------------------------------------------
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            number = rnd.Next(1, 101);
+
+            btnStart.Enabled = false;
+            tbUserInput.Enabled = true;
+            btnCheck.Enabled = true;
+            lblResult.Text = "";
+            tbUserInput.Text = "";
+        }
+
+        //---------------------------------------------------------------------
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int userNumber = int.Parse(tbUserInput.Text);
+                if (userNumber >= 1 && userNumber <= 100)
+                {
+
+                    CheckValue(number, userNumber);
+                }
+                else
+                {
+                    MessageBox.Show("Введите число от 1 до 100");
+                    tbUserInput.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите число от 1 до 100");
+                tbUserInput.Text = "";
+            }
+            
+            
+        }
+
+        //---------------------------------------------------------------------
+        private void CheckValue(int number, int userNumber)
+        {
+            if (userNumber > number)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Введено слишком большое число";
+            }
+            else if (userNumber < number)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Введено слишком маленькое число";
+            }
+            else
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "Поздравляю! Вы угадали";
+
+                btnStart.Enabled = true;
+                tbUserInput.Enabled = false;
+                btnCheck.Enabled = false;
+            }
+            
+        }
+    }
+}
